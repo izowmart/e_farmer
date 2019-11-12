e747f0e714e1d9114c00d1411c1488bb8fefa85e
izowmart <izowmart8@gmail.com>
2019-11-12 22:09:24 +0300

    :books:fixed bugs that originated from facebook sdk dependency everything working fine

--- app/src/main/java/com/example/e_farmer/LoginActivity.java
+++ app/src/main/java/com/example/e_farmer/LoginActivity.java
@@ -54,10 +54,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
 //        google
         signInButton = findViewById(R.id.mGoogleLogin);
